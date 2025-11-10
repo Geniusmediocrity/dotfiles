c5db63d6442744f6f1ebbf977e6ccdf390054a8d
alexey <lesal2767@gmail.com>
2025-11-10 22:50:26 +0300

    Markdown previewer

--- .config/nvim/lua/core/keymaps/live_preview.lua
+++ .config/nvim/lua/core/keymaps/live_preview.lua
@@ -3,3 +3,7 @@ vim.keymap.set("n", "<leader>Lc", ":LivePreview close<CR>", { silent = true, des
 vim.keymap.set("n", "<leader>Lp", ":LivePreview pick<CR>", { silent = true, desc = "[L]ive Preview [P]ick file" })
 vim.keymap.set("n", "<leader>Lh", ":help LivePreview<CR>", { silent = true, desc = "[L]ive Preview [H]elp" })
 vim.keymap.set("n", "<leader>Lp", ":LivePreview pick<CR>", { silent = true, desc = "[L]ive Preview [P]ick" })
+
+vim.keymap.set("n", "<leader>Mp", ":MarkdownPreview<CR>", { silent = true, desc = "[M]arkdown [P]review" })
+vim.keymap.set("n", "<leader>Mt", ":MarkdownPreviewToggle<CR>", { silent = true, desc = "[M]arkdown [T]oggle" })
+vim.keymap.set("n", "<leader>Ms", ":MarkdownPreviewStop<CR>", { silent = true, desc = "[M]arkdown [S]top" })
--- .config/nvim/lua/plugins/live_preview.lua
+++ .config/nvim/lua/plugins/live_preview.lua
@@ -16,4 +16,13 @@ return {
 			picker = "telescope",
 		},
 	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
 }
